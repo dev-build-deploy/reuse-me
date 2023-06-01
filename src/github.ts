c044509e4fc4122e2c314702dc9faa280561ec82
Kevin de Jong <kevin.dejong@tomtom.com>
2023-06-01 07:48:42 +0200

    feat: add GitHub Action to validate your repository and PR
    This commit introduces a basic GitHub Action which can be used to
    validate both your Pull Request content (modified files) or the
    complete repository.

--- src/github.ts
+++ src/github.ts
@@ -1,5 +1,6 @@
 /* 
 SPDX-FileCopyrightText: 2023 Kevin de Jong <monkaii@hotmail.com>
+
 SPDX-License-Identifier: GPL-3.0-or-later
 */
 
@@ -24,6 +25,61 @@ Please refer to the [Reuse FAQ](https://reuse.software/faq/) for more informatio
 - [SPDX License List](https://spdx.org/licenses/)
 - [SPDX Specification](https://spdx.github.io/spdx-spec/)
 `
+
+/**
+ * Repository information
+ * @interface IRepository
+ * @member owner The owner of the repository
+ * @member repo The name of the repository
+ */
+interface IRepository {
+  owner: string
+  repo: string
+}
+
+let REF: string;  // TODO: Use a configuration object instead of a global variable
+let REPOSITORY: IRepository;  // TODO: Use a configuration object instead of a global variable
+const setRepositoryContext = (owner: string, repo: string, ref: string) => {
+  REPOSITORY = { owner: owner, repo: repo }
+  REF = ref;
+}
+
+/**
+ * Retrieves all affected files of the pull request.
+ * @param octokit Octokit instance
+ * @param pullNumber Pull request number
+ * @returns Dictionary of files, seperate by modification type (added, removed, modified)
+ */
+const getPullRequestCommits = async (octokit: any, pullNumber: number) => {
+  const delta: { added: string[], removed: string[], modified: string[] } = { added: [], removed: [], modified: [] };
+  const allFiles: string[] = [];
+
+  for await (
+    const response of octokit.paginate.iterator(
+      octokit.rest.pulls.listFiles,
+      { ...REPOSITORY, pull_number: pullNumber }
+    )
+  ) {
+    for (const file of response.data) {
+      // NOTE: We will only store the latest modification status of the file, 
+      //       as we want to ensure that we do not scan files that have been added
+      //       and removed in the same pull request.
+      if (file.filename in allFiles) {
+        continue;
+      }
+
+      allFiles.push(file.filename);
+
+      switch (file.status) {
+        case "added" || "copied": delta.added.push(file.filename); break;
+        case "modified" || "renamed" || "changed": delta.modified.push(file.filename); break;
+        case "removed": delta.removed.push(file.filename); break;
+      }
+    }
+  }
+
+  return [delta];
+};
 
 /**
  * Retrieves all issues for the repository.
@@ -89,15 +145,16 @@ const updateIssue = async (context: any, issues: any[], file: IFile, header: ISP
 * @returns The contents of the file
 */
 const getFileContents = async (
-  context: any,
+  octokit: any,
   filePath: string
 ): Promise<string> => {
   try {
-    const response = await context.octokit.repos.getContent(context.repo({ path: filePath }));
+    const response = await octokit.repos.getContent({ ...REPOSITORY, path: filePath, ref: REF });
     return Buffer.from(response.data.content, "base64").toString();
   } catch (error) {
+    // TODO: Improve error handling, for now we assume an empty file if the file could not be retrieved
     return "";
   }
 };
 
-export { getFileContents, getIssues, updateIssue }
+export { getPullRequestCommits, getFileContents, getIssues, setRepositoryContext, updateIssue, IRepository }
