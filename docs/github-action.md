5e6f9af359876f79598b22367509beb7977e0c5e
Kevin de Jong <monkaii@hotmail.com>
2023-06-15 14:21:39 +0200

    feat!: extend the validation requirements
    This commit is adding the following requirement checks to ReuseMe:
    
    - Each Covered File MUST have Copyright and Licensing Information associated with it
    - The SPDX License Identifier (X) MUST be LicenseRef-[letters, numbers, ".", or "-"] as defined by the SPDX Specification
    - The Project MUST include a License File for every license, but is missing X
    - The Project MUST NOT include License Files for licenses under which none of the files in the Project are licensed.
    
    The extended rules required me to drop support for scanning modified files; reducing
    the overall complexity of the implementation. However, this will require users
    to checkout the git repo in CI context

--- docs/github-action.md
+++ docs/github-action.md
@@ -6,13 +6,13 @@ SPDX-License-Identifier: GPL-3.0-or-later
 
 # GitHub Action
 
-You can scan your [full repository](#repository-scanning) and [pull requests](#pull-request-scanning) for determining compliance with the [Reuse specification]. Running this GitHub Action will result in output similar to:
+You can scan your [full repository](#repository-scanning) for determining compliance with the [Reuse specification].
+
+Running this GitHub Action will result in output similar to:
 
 <img src="./images/cli_example.svg">
 
 ## Workflows
-
-### Repository Scanning
 
 You can enable a full repository scan, regardless of which files are modified in the current context. This can be particularly useful in case you want to ensure full compliance with the [Reuse specification].
 
@@ -34,47 +34,3 @@ jobs:
       # Run the action to validate the contents
       - uses: dev-build-deploy/reuse-me@v0
 ```
-
-### Pull Request Scanning
-
-You can limit the validation to files affected within your current Pull Request. This could be useful for legacy projects in which you want to gradually introduce the [Reuse specification].
-
-In addition, this can have a (positive) impact on large repositories with a rich history.
-
-#### Workflow
-
-```yaml
-name: Copyright & Licenses
-on:
-  pull_request:
-
-permissions:
-  contents: read
-  pull-requests: read
-
-jobs:
-  reuse-me:
-    name: REUSE compliance
-    runs-on: ubuntu-latest
-    steps:
-      - uses: dev-build-deploy/reuse-me@v0
-        with:
-          token: ${{ github.token }}
-```
-
-### Inputs
-
-| Name | Required | Description |
-| --- | --- | --- |
-| `token` | *NO* | GitHub token only used for [pull request scanning](#pull-request-scanning) and can be ommitted during a [full repository scan](#repository-scanning). Please refer to [permissions](#permissions) for more details on the permissions required for the provided GitHub token. |
-
-### Permissions
-
-The following permissions are only required in case you are performing a [pull request scan](#pull-request-scanning).
-
-| Name | Value | Comment |
-| --- | --- | --- |
-| `contents` | `read` | The GitHub Action will read contents from your repository in order to check for valid REUSE headers in your modified files |
-| `pull-requests` | `read` | In order to limit scans to modified files ONLY, the GitHub Action needs to have read access to your PR data |
-
-[Reuse specification]: https://reuse.software/spec/
--- src/validator.ts
+++ src/validator.ts
@@ -5,41 +5,59 @@ SPDX-License-Identifier: GPL-3.0-or-later
 */
 
 import { IValidationResult } from "./interfaces";
-import { ExpressiveMessage } from "@dev-build-deploy/diagnose-it";
 
 import * as spdx from "./spdx";
+import { fileRequirements, projectRequirements } from "./requirements";
 
-const validate = (sbom: spdx.SoftwareBillOfMaterials): IValidationResult[] => {
+/**
+ * Validates all files against requirements related to File contents
+ * @param sbom The SBOM containing the files to validate
+ * @returns List of validation results
+ */
+export function validateFiles(sbom: spdx.SoftwareBillOfMaterials): IValidationResult[] {
   const results: IValidationResult[] = [];
 
   for (const file of sbom.files) {
     const result: IValidationResult = {
       file: file,
-      compliant: spdx.isReuseCompliant(file),
+      compliant: true,
       errors: [],
     };
 
-    if (result.compliant === false) {
-      if (spdx.hasValidLicense(file) === false)
-        result.errors.push(
-          new ExpressiveMessage()
-            .id(file.fileName)
-            .error("Each Covered File MUST have Licensing Information associated with it.")
-            .toString()
-        );
-      if (spdx.hasValidCopyrightText(file) === false)
-        result.errors.push(
-          new ExpressiveMessage()
-            .id(file.fileName)
-            .error("Each Covered File MUST have Copyright Information associated with it.")
-            .toString()
-        );
+    for (const req of fileRequirements) {
+      const errors = req.validate(file);
+      if (errors === undefined) continue;
+
+      result.compliant = false;
+      result.errors.push(...errors.errors.map(e => e.toString()));
     }
 
     results.push(result);
   }
 
   return results;
-};
+}
 
-export { validate };
+/**
+ * Validates the SBOM against requirements related to the project
+ * @param sbom The SBOM (Project) to validate
+ * @param licenses List of licenses stored in LICENSES/
+ * @returns Validation result
+ */
+export function validateSBOM(sbom: spdx.SoftwareBillOfMaterials, licenses: string[]): IValidationResult {
+  const result: IValidationResult = {
+    file: sbom.files[0],
+    compliant: true,
+    errors: [],
+  };
+
+  for (const req of projectRequirements) {
+    const errors = req.validate(sbom, licenses);
+    if (errors === undefined) continue;
+
+    result.compliant = false;
+    result.errors.push(...errors.errors.map(e => e.toString()));
+  }
+
+  return result;
+}
